a5565a2f6ac1f62d1195fd95a6e237d04a389273
Dr Blinken <drblinken@gmail.com>
2012-12-13 21:18:39 +0100

    created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poll::Application.config.secret_token = '9e21cb228cee6dc906d916527cef130aea8e7db900507968c4df41d099c66af1dedc635e9d0ea64d69a9f2e4ccf502f4b4f4a8d6946c4518fc22718ec752c099'
